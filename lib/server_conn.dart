8acd878a04a0244ab4003e71f2e25c2330b0e0ce
Sayuni Ranawaka <ranawakarasd.21@itfac.mrt.ac.lk>
2024-04-23 15:57:51 +0530

    establish connection between passenger app and backend

--- lib/server_conn.dart
+++ lib/server_conn.dart
@@ -1,7 +1,12 @@
 import 'package:http/http.dart' as http;
 
+/// Fetches server data from the specified URL.
+///
+/// Makes an HTTP GET request to the given URL and prints the response body if the request is successful.
+/// If the request fails, it prints the status code.
 void fetchServerData() async {
-  var url = Uri.parse('https://zw08kknv-3000.inc1.devtunnels.ms/data');
+  var url = Uri.parse(
+      'https://zw08kknv-3000.inc1.devtunnels.ms/data'); //TODO: Replace URL with cloud server URL
   var response = await http.get(url);
   if (response.statusCode == 200) {
     print('Response from Node.js: ${response.body}');
