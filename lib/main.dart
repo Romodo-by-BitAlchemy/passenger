6896ad22c6c9e0fb45e0ee92d6d7bcaa97fe4ce9
Kavindhya Sammani Rajasooriya <rajasooriyaks.21@uom.lk>
2024-07-08 10:45:24 +0530

    chore: Update intl dependency to version 0.19.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:passenger/pages/fpassword.dart';
 import 'package:passenger/pages/getemail.dart';
 import 'package:passenger/pages/login.dart';
@@ -7,6 +8,7 @@ import 'package:passenger/pages/rpassword.dart';
 import 'package:passenger/pages/signup.dart';
 import 'package:passenger/pages/updatep.dart';
 import 'package:passenger/components/bottom_nav_bar.dart';
+import 'package:passenger/Pages/book_trip.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,65 +17,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      
-      
-  routes: {
-    '/login': (context) => const LoginScreen(title: '',),
-    '/signup': (context) => const SignUpScreen(),
-    '/fpassword': (context) => ForgotPassword(),
-    '/rpassword': (context) => const ResetPassword(token: ''),
-    '/dashboard': (context) => const MyHomePage(title: 'Dashboard'),
-    //'/logout': (context) =>  Logout(),
-     '/updatePassenger': (context) => UpdatePassengerScreen(
-      passengerEmail: ModalRoute.of(context)!.settings.arguments as String,
-    ),
-     '/get_email': (context) => GetEmailScreen(),
-    
-  },
-  initialRoute: '/login', // Set the initial route
-);
-
-    
+      routes: {
+        '/login': (context) => const LoginScreen(title: ''),
+        '/signup': (context) => const SignUpScreen(),
+        '/fpassword': (context) => ForgotPassword(),
+        '/rpassword': (context) => const ResetPassword(token: ''),
+        '/dashboard': (context) => const MyHomePage(title: 'Dashboard'),
+        //'/logout': (context) =>  Logout(),
+        '/updatePassenger': (context) => UpdatePassengerScreen(
+          passengerEmail: ModalRoute.of(context)!.settings.arguments as String,
+        ),
+        '/get_email': (context) => GetEmailScreen(),
+        '/tripBook': (context) => const TripBookPage(), // Add the route for trip book page
+      },
+      initialRoute: '/login',
+    );
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -82,17 +53,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   int _selectedIndex = 0;
+  String? _firstName;
+  DateTime nextTripDate = DateTime(2024, 7, 10, 10, 0); // Dummy next trip date and time
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _fetchPassengerDetails();
+  }
+
+  Future<void> _fetchPassengerDetails() async {
+    // Replace this with actual database call
+    await Future.delayed(const Duration(seconds: 2));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _firstName = 'Sammani'; // Dummy first name
     });
   }
 
@@ -116,58 +91,114 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  String _calculateTimeToStart() {
+    Duration difference = nextTripDate.difference(DateTime.now());
+    if (difference.isNegative) {
+      return "Trip already started";
+    }
+    return "${difference.inDays} days ${difference.inHours.remainder(24)} hours ${difference.inMinutes.remainder(60)} minutes";
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              if (_firstName != null)
+                Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.person, color: Theme.of(context).primaryColor),
+                      const SizedBox(width: 8.0),
+                      Text(
+                        'Welcome, $_firstName',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                ),
+              const SizedBox(height: 20.0),
+              Card(
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: Icon(Icons.directions_bus, color: Theme.of(context).primaryColor),
+        title: const Text('Next Trip'),
+        subtitle: Text(
+          // Corrected date and time formatting
+          'Date: ${DateFormat('yyyy-MM-dd').format(nextTripDate.toLocal())} \nTime: ${DateFormat('hh:mm a').format(nextTripDate.toLocal())}\nTime to start: ${_calculateTimeToStart()}',
+        ),
+        trailing: ElevatedButton(
+          onPressed: () {
+            // Handle track vehicle action
+          },
+          child: const Text('Track Vehicle'),
+        ),
+      ),
+    ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/tripBook'); // Navigate to trip book page
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 165, 131, 229), // Light purple color
+                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    '+ Book a Trip',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold , color: Color.fromARGB(248, 67, 59, 59)),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                'Upcoming Trips',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 10.0),
+              Card(
+                elevation: 4,
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                child: ListTile(
+                  leading: Icon(Icons.directions_bus, color: Theme.of(context).primaryColor),
+                  title: const Text('Trip to Nugegoda'),
+                  subtitle: const Text('Date: 2024-07-10\nTime: 10:00 AM'),
+                ),
+              ),
+              Card(
+                elevation: 4,
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                child: ListTile(
+                  leading: Icon(Icons.directions_bus, color: Theme.of(context).primaryColor),
+                  title: const Text('Trip to Battaramulla'),
+                  subtitle: const Text('Date: 2024-07-15\nTime: 02:00 PM'),
+                ),
+              ),
+              // Add more dummy trips here
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          // Handle chat action
+        },
+        child: const Icon(Icons.chat),
+      ),
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
         onItemSelected: _onItemTapped,
